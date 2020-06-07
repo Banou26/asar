defc67c56c06d6a6353478a89ef141d3268182e1
Banou26 <me@banou.dev>
2020-06-07 22:31:06 +0200

    chore: 🔧make the package importable from parcel

--- src/index.ts
+++ src/index.ts
@@ -31,52 +31,52 @@ export {
 //   'bar.txt': 'qux quux quuux'
 // }).then(v => console.log(saveByteArray('packed', v)))
 
-createPackage({
-  'New folder': {
-    'foo.txt': 'foo bar baz aaaa',
-    'bar.txt': 'qux quux quuux'
-  },
-  'foo.txt': 'foo bar baz aaaa',
-  'bar.txt': 'qux quux quuux'
-}).then(v => console.log(v))
+// createPackage({
+//   'New folder': {
+//     'foo.txt': 'foo bar baz aaaa',
+//     'bar.txt': 'qux quux quuux'
+//   },
+//   'foo.txt': 'foo bar baz aaaa',
+//   'bar.txt': 'qux quux quuux'
+// }).then(v => console.log(v))
 
-createPackage({
-  '/New folder/foo.txt': 'foo bar baz aaaa',
-  '/New folder/bar.txt': 'qux quux quuux',
-  '/foo.txt': 'foo bar baz aaaa',
-  '/bar.txt': 'qux quux quuux'
-}, { flat: true }).then(v => console.log(v))
+// createPackage({
+//   '/New folder/foo.txt': 'foo bar baz aaaa',
+//   '/New folder/bar.txt': 'qux quux quuux',
+//   '/foo.txt': 'foo bar baz aaaa',
+//   '/bar.txt': 'qux quux quuux'
+// }, { flat: true }).then(v => console.log(v))
 
 
 
-//? EXTRACTING TEST CODE
+// //? EXTRACTING TEST CODE
 
-const base64Pkg = `\
-BAAAALwAAAC4AAAAswAAAHsiZmlsZXMiOnsiYmFyLnR4dCI6eyJzaXplIjoxNCw\
-ib2Zmc2V0IjoiMCJ9LCJmb28udHh0Ijp7InNpemUiOjE2LCJvZmZzZXQiOiIxNC\
-J9LCJOZXcgZm9sZGVyIjp7ImZpbGVzIjp7ImJhci50eHQiOnsic2l6ZSI6MTQsI\
-m9mZnNldCI6IjMwIn0sImZvby50eHQiOnsic2l6ZSI6MTYsIm9mZnNldCI6IjQ0\
-In19fX19AHF1eCBxdXV4IHF1dXV4Zm9vIGJhciBiYXogYWFhYXF1eCBxdXV4IHF\
-1dXV4Zm9vIGJhciBiYXogYWFhYQ==`
-const packedPkg = new Blob([Buffer.from(base64Pkg, 'base64')])
+// const base64Pkg = `\
+// BAAAALwAAAC4AAAAswAAAHsiZmlsZXMiOnsiYmFyLnR4dCI6eyJzaXplIjoxNCw\
+// ib2Zmc2V0IjoiMCJ9LCJmb28udHh0Ijp7InNpemUiOjE2LCJvZmZzZXQiOiIxNC\
+// J9LCJOZXcgZm9sZGVyIjp7ImZpbGVzIjp7ImJhci50eHQiOnsic2l6ZSI6MTQsI\
+// m9mZnNldCI6IjMwIn0sImZvby50eHQiOnsic2l6ZSI6MTYsIm9mZnNldCI6IjQ0\
+// In19fX19AHF1eCBxdXV4IHF1dXV4Zm9vIGJhciBiYXogYWFhYXF1eCBxdXV4IHF\
+// 1dXV4Zm9vIGJhciBiYXogYWFhYQ==`
+// const packedPkg = new Blob([Buffer.from(base64Pkg, 'base64')])
 
-extractFile(packedPkg, '/New folder/foo.txt')
-  .then(v => console.log(v.toString()))
+// extractFile(packedPkg, '/New folder/foo.txt')
+//   .then(v => console.log(v.toString()))
 
-listPackage(packedPkg).then(v => console.log(v))
-listPackage(packedPkg, { flat: true }).then(v => console.log(v))
+// listPackage(packedPkg).then(v => console.log(v))
+// listPackage(packedPkg, { flat: true }).then(v => console.log(v))
 
-const stringifyPackageValues = (v: Object): { [key: string]: string } =>
-  Object.fromEntries(
-    Object
-    .entries(v)
-    .map(([key, val]) => [
-      key,
-      typeof val === 'object' && val.constructor === Object
-        ? stringifyPackageValues(val)
-        : val.toString()
-    ])
-  )
+// const stringifyPackageValues = (v: Object): { [key: string]: string } =>
+//   Object.fromEntries(
+//     Object
+//     .entries(v)
+//     .map(([key, val]) => [
+//       key,
+//       typeof val === 'object' && val.constructor === Object
+//         ? stringifyPackageValues(val)
+//         : val.toString()
+//     ])
+//   )
 
-extractAll(packedPkg).then(v => console.log(stringifyPackageValues(v)))
-extractAll(packedPkg, { flat: true }).then(v => console.log(stringifyPackageValues(v)))
+// extractAll(packedPkg).then(v => console.log(stringifyPackageValues(v)))
+// extractAll(packedPkg, { flat: true }).then(v => console.log(stringifyPackageValues(v)))
